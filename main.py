c23a0c50476c03061576df0cd81a901e517b16b7
Dinusha <dinusha.diss@gmail.com>
2023-08-08 19:36:17 +0100

    Emailing Monday Motivational Quotes

--- main.py
+++ main.py
@@ -2,15 +2,15 @@
 #
 # import smtplib
 #
-# my_email = "addinusha@gmail.com"
-# password = "wmfeywuitgyjmnen"
+# my_email = "your_email@gmail.com"
+# password = "ernejrhdckhnmfdk"  # App Password - gmail account
 #
 # with smtplib.SMTP("smtp.gmail.com") as connection:
 #     connection.starttls()
 #     connection.login(user=my_email, password=password)
 #     connection.sendmail(
 #         from_addr=my_email,
-#         to_addrs="dinusha.diss@gmail.com",
+#         to_addrs="receiver's_email@gmail.com",
 #         msg="Subject:Testing Python Mail\n\nHello, this is the email body"
 #     )
 #
@@ -22,8 +22,8 @@ import datetime as dt
 import random
 
 data_dict = {}
-my_email = "addinusha@gmail.com"
-password = "wmfeywuitgyjmnen"
+my_email = "your_email@gmail.com"
+password = "ernejrhdckhnmfdk"
 now = dt.datetime.now()
 
 if now.weekday() == 1:
@@ -36,6 +36,6 @@ if now.weekday() == 1:
         connection.login(user=my_email, password=password)
         connection.sendmail(
             from_addr=my_email,
-            to_addrs="dinusha.diss@gmail.com",
+            to_addrs="receiver's_email@gmail.com",
             msg=f"Subject: Monday Motivation!\n\n{quote}"
         )
